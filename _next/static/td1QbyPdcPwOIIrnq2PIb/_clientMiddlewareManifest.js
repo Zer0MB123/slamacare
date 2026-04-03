d587f2227b5707572b7faa444c6868bcb3492b6b
A16 <93436479+Zer0MB123@users.noreply.github.com>
2026-04-03 18:15:29 +0400

    Delete _next/static directory

--- _next/static/td1QbyPdcPwOIIrnq2PIb/_clientMiddlewareManifest.js
+++
@@ -1,6 +0,0 @@
-self.__MIDDLEWARE_MATCHERS = [
-  {
-    "regexp": "^\\/slamacare(?:\\/(_next\\/data\\/[^/]{1,}))?(?:\\/((?!api|_next\\/static|_next\\/image|favicon.ico|public|products|.*\\.svg|.*\\.png|.*\\.jpg).*))(\\.json)?[\\/#\\?]?$",
-    "originalSource": "/((?!api|_next/static|_next/image|favicon.ico|public|products|.*\\.svg|.*\\.png|.*\\.jpg).*)"
-  }
-];self.__MIDDLEWARE_MATCHERS_CB && self.__MIDDLEWARE_MATCHERS_CB()
